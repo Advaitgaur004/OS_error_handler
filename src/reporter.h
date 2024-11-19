fe764b4c14a0b31148d7e52c7869c455a7c0ab31
Advait Gaur <b22cs004@iitj.ac.in>
2024-11-19 15:17:03 +0530

    Remove email reporting functionality: delete reporter.c and reporter.h files

--- src/reporter.h
+++
@@ -1,9 +0,0 @@
-// File: src/reporter.h
-#ifndef REPORTER_H
-#define REPORTER_H
-
-#include "error_handler.h"
-
-void report_error(ErrorType type, const char *message, int error_code);
-
-#endif // REPORTER_H
